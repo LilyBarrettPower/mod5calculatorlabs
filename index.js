eb57ee927b49eb36ae64bf40e83d16f21f045a8a
Lily Barrett <11barrettpl@gmail.com>
2023-10-05 10:34:14 +1300

    lab 1

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+// include express using require
 const express = require('express');
-const calculatorRoute = require('./routes/calculatorRoute');
+// include the calculator routes using require and adding to a variable
+const calculatorRoutes = require('./routes/calculatorRoute');
 
+// create the server application using express()
 const serverApp = express();
+// choose what port you want your server to run on
 const port = 3000;
 
-serverApp.use('/Calculator', calculatorRoute)
+// map the route you want your calculator to use /Add, /Multiply etc will come after the /Calculator
+serverApp.use('/Calculator', calculatorRoutes)
 
+// start the server and log a message to the console to ensure the server is working correctly 
 serverApp.listen(port, () => console.log(`Server running on port ${port}`));
--- routes/calculatorRoute.js
+++ routes/calculatorRoute.js
@@ -1,14 +1,48 @@
+// Install express using "require"
 const express = require('express');
+// Create the router using express.Router() method from express 
 const router = express.Router();
 
-router.get('/Add', (req, res) => {
+// create the addition router
+router.get('/Add', (req, res) => { //You will need to include /Add in your query string 
+    let num1 = parseInt(req.query.num1); // parseInt so the numbers are parsed as number not strings 
+    let num2 = parseInt(req.query.num2);
+
+    let result = num1 + num2; // because its an addition route 
+
+    res.status(200); // make sure the status is OK
+    res.send({ res: result }); // return the result in json format 
+});
+
+router.get('/Multiply', (req, res) => {
     let num1 = parseInt(req.query.num1);
     let num2 = parseInt(req.query.num2);
 
-    let result = num1 + num2;
+    let result = num1 * num2;
 
-    res.status(200)
-    res.send({ res: result })
+    res.status(200);
+    res.json({ res: result });
 });
 
+router.get('/Divide', (req, res) => {
+    let num1 = parseInt(req.query.num1);
+    let num2 = parseInt(req.query.num2);
+
+    let result = num1 / num2;
+
+    res.status(200);
+    res.json({ res: result });
+});
+
+router.get('/Subtract', (req, res) => {
+    let num1 = parseInt(req.query.num1);
+    let num2 = parseInt(req.query.num2);
+
+    let result = num1 - num2;
+
+    res.status(200);
+    res.json({ res: result });
+});
+
+// export your router to be used in the index.js file
 module.exports = router;
