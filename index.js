e9048952cae4da04bd317427fdc38c43f2dedaea
Lily Barrett <11barrettpl@gmail.com>
2023-10-07 10:05:44 +1300

    Q3 done

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = 3000;
 
 // map the route you want your calculator to use /Add, /Multiply etc will come after the /Calculator
 serverApp.use('/Calculator', calculatorRoutes)
+// map to the static html page to display the calculator interface 
+serverApp.use('/', express.static('Public'))
 
 // start the server and log a message to the console to ensure the server is working correctly 
-serverApp.listen(port, () => console.log(`Server running on port ${port}`));
+serverApp.listen(port, () => console.log(`Server running on http://localhost:${port}`));
