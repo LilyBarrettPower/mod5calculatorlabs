a298766c5c02805d6c0caa173cd4462595bf9c9d
Lily Barrett <11barrettpl@gmail.com>
2023-10-09 07:55:20 +1300

    added comments

--- controllers/calculatorController.js
+++ controllers/calculatorController.js
@@ -1,11 +1,15 @@
+// import the calculator library using require 
 const CalculatorLibrary = require('../libraries/calculator');
-
+// create a new instance of the calculator library and assign to a variable. is able to access the calculator library methods 
 let calculator = new CalculatorLibrary();
 
+// create a function for adding the numbers 
 const addNumbers = (req, res) => {
+    // call the add method from the library on the instance
+    // the parameters are number 1 and number 2 inputted by user 
     let result = calculator.add(req.query.num1, req.query.num2);
     res.status(200); // make sure the status is OK
-    res.send({ res: result });
+    res.send({ res: result }); // return the result of the calulation
 }
 const multiplyNumbers = (req, res) => {
     let result = calculator.multiply(req.query.num1, req.query.num2);
@@ -23,4 +27,5 @@ const divideNumbers = (req, res) => {
     res.json({ res: result });
 }
 
+// export the functions for doing calculations
 module.exports = { addNumbers, multiplyNumbers, subtractNumbers, divideNumbers };
